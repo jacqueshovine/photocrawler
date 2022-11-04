d0dedabb75263a9460ae89a368ba64d5e345ac94
Jacques Hovine <jacques.hovine@partoo.fr>
2022-11-04 17:07:11 +0100

    create functional login function

--- main.py
+++ main.py
@@ -3,5 +3,25 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 import config
 
+def login(mail, password):
+    login_mail_input = driver.find_element(By.NAME, "email")
+    login_password_input = driver.find_element(By.NAME, "password")
+    login_submit = driver.find_element(By.XPATH, "//input[@type='submit']")
+
+    login_mail_input.send_keys(mail)
+    login_password_input.send_keys(password)
+    login_submit.click()
+
+
 driver = webdriver.Firefox(executable_path=config.geckodriver_path)
-driver.get("http://www.python.org")
+driver.get("https://www.photofeeler.com/vote/dating")
+
+login(config.login, config.password)
+
+# karma_gauge = driver.find_element(By.CSS_SELECTOR, "div.karma-bar")
+
+
+# driver.close()
+
+
+
